c780724e9bb6306838de14c5359df26868b55274
ChatDo <nicolas.julie@epitech.eu>
2022-03-16 14:43:15 +0400

    :ambulance: Re-arranged Makefiles
    ok

--- games/nibbler/Nibbler.cpp
+++
@@ -1,8 +0,0 @@
-/*
-** EPITECH PROJECT, 2022
-** B-OOP-400-RUN-4-1-arcade-nicolas.reboule
-** File description:
-** Nibbler
-*/
-
-#include "Nibbler.hpp"
--- games/pacman/Pacman.cpp
+++
@@ -1,9 +0,0 @@
-/*
-** EPITECH PROJECT, 2022
-** B-OOP-400-RUN-4-1-arcade-nicolas.reboule
-** File description:
-** Pacman
-*/
-
-#include "Pacman.hpp"
-
